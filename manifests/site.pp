6b2d827c8146584afe60be50b0e6eeb6f62e5914
James Hutchison <delphius411@hotmail.com>
2021-11-10 17:48:19 -0700

    Added profiles and classes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,12 @@
 node default {
-  
+
 }
 node 'client1.lab.net' {
-  
+  include role::master_server
 }
 
 node 'netsvcs.lab.net' {
+  
   file {'/root/README':
     ensure  => file,
     content => 'This is a readme created by puppet.',
