6d7e021d91362a37429529ffccdc6aa540394570
James Hutchison <delphius411@hotmail.com>
2021-11-15 16:05:29 -0700

    fixed missing single quotes

--- site/ubuntu_stig/manifests/init.pp
+++ site/ubuntu_stig/manifests/init.pp
@@ -14,18 +14,18 @@ class ubuntu_stig {
 
   file { '/etc/pam_pkcs11':
       ensure => directory,
-      mode => 755,
+      mode => '755',
   }
 
   file { '/etc/pam_pkcs11/pam_pkcs11.conf':
       ensure  => file,
-      mode    => 755,
+      mode    => '755',
       source => 'puppet:///modules/ubuntu_stig/login.defs',
   }
 
   file { '/etc/login.defs':
       ensure => file,
-      mode => 755,
+      mode => '755',
       source => 'puppet:///modules/ubuntu_stig/login.defs',
   }
 }
