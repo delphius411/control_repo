cd004e708911451f49f4c1b46352cd98c26d2bf4
James Hutchison <delphius411@hotmail.com>
2021-11-15 18:06:11 -0700

    More stig settings applied

--- site/ubuntu_stig/manifests/init.pp
+++ site/ubuntu_stig/manifests/init.pp
@@ -8,6 +8,15 @@ class ubuntu_stig {
       ensure => present,
   }
 
+  package { 'ssh':
+    ensure => present,
+  }
+
+  service { 'ssh':
+    ensure => running,
+    enable => true,
+  }
+
   package { 'libpam-pkcs11':
       ensure => present,
   }
@@ -21,6 +30,12 @@ class ubuntu_stig {
       ensure  => file,
       mode    => '755',
       source => 'puppet:///modules/ubuntu_stig/login.defs',
+  }
+
+  file { '/etc/ssh/sshd_config':
+      ensure => file,
+      mode => '755',
+      source => 'puppet:///modules/ubuntu_stig/sshd_config',
   }
 
   file { '/etc/login.defs':
@@ -45,6 +60,42 @@ class ubuntu_stig {
     line => 'TMOUT=600',
   }
 
+  file_line { 'pam_common_auth_pkcs11':
+      path => '/etc/pam.d/common-auth',
+      line => 'auth [success=2 default=ignore] pam_pkcs11.so',
+  }
 
+  file { '/etc/issue.net':
+        ensure => file,
+        mode => '755',
+        source => 'puppet:///modules/ubuntu_stig/issue.net',
+  }
+
+  package {'libpam-pwquality':
+    ensure => present
+    before => File['/etc/security/pwquality.conf'],
+  }
+
+  file { '/etc/security/pwquality.conf':
+      ensure  => file,
+      mode    => '755',
+      source => 'puppet:///modules/ubuntu_stig/pwquality.conf',
+  }
+
+  package {'opensc-pkcs11':
+    ensure => present
+  }
+
+  file { '/etc/pam.d/common-password':
+      ensure  => file,
+      mode    => '755',
+      source => 'puppet:///modules/ubuntu_stig/common-password',
+  }
+
+  file { '/etc/security/faillock.conf':
+      ensure  => file,
+      mode    => '755',
+      source => 'puppet:///modules/ubuntu_stig/faillock.conf',
+  }
 
 }
