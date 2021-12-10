6f3e689352ce4b77ed43e2a7f57082087a0787c4
James Hutchison <delphius411@hotmail.com>
2021-12-10 10:55:45 -0700

    tuning r10k settings

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,9 +1,10 @@
 # This class is used to setup webhooks to a Github repo
 class profile::r10k {
-  class {'r10k':
+  class { 'r10k':
     remote => 'https://github.com/delphius411/control_repo.git',
   }
-  class {'r10k::webhook::config':
+
+  class { 'r10k::webhook::config':
     use_mcollective => false,
     enable_ssl      => false,
   }
